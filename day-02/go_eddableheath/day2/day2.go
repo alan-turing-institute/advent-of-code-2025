a3aba8e189256c21f710b28cb1074ad4fed1d63f
eddableheath <edable-heath@turing.ac.uk>
2025-12-02 09:58:06 +0000

    day2 solutions

--- day-02/go_eddableheath/day2/day2.go
+++ day-02/go_eddableheath/day2/day2.go
@@ -53,7 +53,7 @@ func Part1(inputPath string) (interface{}, error) {
 
 	var repeatedNums []int
 	for _, pair := range numPairs {
-		repeatedNums = append(repeatedNums, checkRepeatedDigits(pair[0], pair[1])...)
+		repeatedNums = append(repeatedNums, checkRepeatedDigitsPattern1(pair[0], pair[1])...)
 	}
 
 	sum := 0
@@ -64,7 +64,7 @@ func Part1(inputPath string) (interface{}, error) {
 	return sum, nil
 }
 
-func checkRepeatedDigits(num1, num2 int) []int {
+func checkRepeatedDigitsPattern1(num1, num2 int) []int {
 	/* Check if a numbers in a given range have repeated digits or groups of digits */
 
 	var repeatedNums []int
@@ -76,8 +76,8 @@ func checkRepeatedDigits(num1, num2 int) []int {
 			firstHalf := numStr[:half]
 			secondHalf := numStr[half:]
 			if firstHalf == secondHalf {
-				// fmt.Printf("Found repeated digits: %d\n", i)
 				repeatedNums = append(repeatedNums, i)
+				continue
 			}
 		}
 	}
@@ -85,7 +85,98 @@ func checkRepeatedDigits(num1, num2 int) []int {
 	return repeatedNums
 }
 
+func checkRepeatedDigitsPattern2(num1, num2 int) []int {
+	/* Check if a numbers in a given range have repeated digits or groups of digits */
+
+	var repeatedNums []int
+	for i := num1; i <= num2; i++ {
+		digitCount := len(strconv.Itoa(i))
+		numStr := strconv.Itoa(i)
+		for j := 2; j <= digitCount; j++ {
+			if digitCount%j == 0 {
+				var numberPatterns []string
+				splitSize := digitCount / j
+				for k := 0; k < j; k++ {
+					pattern := numStr[k*splitSize : (k+1)*splitSize]
+					numberPatterns = append(numberPatterns, pattern)
+				}
+				// fmt.Println(numberPatterns)
+				if allSameStrings(numberPatterns) {
+					fmt.Printf("Found repeated pattern %s in number %d\n", numberPatterns[0], i)
+					repeatedNums = append(repeatedNums, i)
+					break
+				}
+				continue
+			}
+		}
+	}
+
+	return repeatedNums
+}
+
+func allSameStrings(a []string) bool {
+	for _, v := range a {
+		if v != a[0] {
+			return false
+		}
+	}
+	return true
+}
+
 func Part2(inputPath string) (interface{}, error) {
-	/* Placeholder for day 2 part 2 solution */
-	return nil, nil
+	/*
+		Part 2 solution:
+
+		Now we need to check for all repeated patterns in range XXXX-YYYY, not just those that split the number
+		in half. I.e for a 6 digit number we need to check:
+
+			- AAAAAA
+			- ABABAB
+			- AABAAB
+			- ABAABA
+			- ABBABB
+
+		We can check these starting by halving the number, then checking for patterns of length from half the
+		number of digits down to 2 (1 is captured by half).
+
+		Then return the sum of the numbers that matched.
+	*/
+
+	// Read input
+	input, err := utils.ReadInputFile(inputPath)
+	if err != nil {
+		fmt.Printf("error here")
+		return nil, err
+	}
+	fmt.Printf("Successfully read input\n")
+
+	numRanges := strings.Split(input[0], ",")
+	var numPairs [][]int
+	for _, numRange := range numRanges {
+		bounds := strings.Split(numRange, "-")
+		var pair []int
+		b1, err := strconv.Atoi(bounds[0])
+		if err != nil {
+			return nil, err
+		}
+		b2, err := strconv.Atoi(bounds[1])
+		if err != nil {
+			return nil, err
+		}
+		pair = append(pair, b1)
+		pair = append(pair, b2)
+		numPairs = append(numPairs, pair)
+	}
+
+	var repeatedNums []int
+	for _, pair := range numPairs {
+		repeatedNums = append(repeatedNums, checkRepeatedDigitsPattern2(pair[0], pair[1])...)
+	}
+
+	sum := 0
+	for _, val := range repeatedNums {
+		sum += val
+	}
+
+	return sum, nil
 }
