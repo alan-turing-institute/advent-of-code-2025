b0f3cc6eb788dd77da46059120cdaf521c1e2111
James Bishop <jbishop@turing.ac.uk>
2025-12-08 12:45:58 +0000

    lannelin day 4

---
+++ day-04/go_lannelin/day_04_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+)
+
+const (
+	inp = `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
+)
+
+func TestPart1(t *testing.T) {
+	want := 13
+	got := Part1(inp)
+	if got != want {
+		t.Errorf("part1(%q) = %d, want %d", inp, got, want)
+	}
+
+}
+
+func TestPart2(t *testing.T) {
+	want := -1
+	got := Part2(inp)
+	if got != want {
+		t.Errorf("part2(%q) = %d, want %d", inp, got, want)
+	}
+
+}
