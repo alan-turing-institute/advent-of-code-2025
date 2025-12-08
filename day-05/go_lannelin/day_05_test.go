3c99aada1f7501fe927508a187fdb6ffc339f96c
James Bishop <jbishop@turing.ac.uk>
2025-12-08 13:44:18 +0000

    lannelin day 5

--- day-05/go_lannelin/day_05_test.go
+++ day-05/go_lannelin/day_05_test.go
@@ -5,20 +5,21 @@ import (
 )
 
 const (
-	inp = `..@@.@@@@.
-@@@.@.@.@@
-@@@@@.@.@@
-@.@@@@..@.
-@@.@@@@.@@
-.@@@@@@@.@
-.@.@.@.@@@
-@.@@@.@@@@
-.@@@@@@@@.
-@.@.@@@.@.`
+	inp = `3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32`
 )
 
 func TestPart1(t *testing.T) {
-	want := 13
+	want := 3
 	got := Part1(inp)
 	if got != want {
 		t.Errorf("part1(%q) = %d, want %d", inp, got, want)
@@ -27,7 +28,7 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	want := 43
+	want := 14
 	got := Part2(inp)
 	if got != want {
 		t.Errorf("part2(%q) = %d, want %d", inp, got, want)
