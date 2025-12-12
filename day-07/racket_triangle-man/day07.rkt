10674f6ef0cc2fdbf36836100707a464697ddf99
James Geddes <jgeddes@turing.ac.uk>
2025-12-12 16:22:06 +0000

    Day 7 in Racket

--- day-07/racket_triangle-man/day07.rkt
+++ day-07/racket_triangle-man/day07.rkt
@@ -2,14 +2,57 @@
 
 (require racket/list/grouping)
 
+(module+ main
 
-(define (propagate-beams beams splitter-rows)
+  (define *puzzle*
+    (with-input-from-file "input.txt" parse-input))
+
+  ;; Part 1
+  (propagate-beams (hash (caar *puzzle*) 1) (cdr *puzzle*) (λ (c1) (λ (c2) 1)))
+
+  ;; Part 2
+  (define-values (_ *beams*)
+    (propagate-beams (hash (caar *puzzle*) 1) (cdr *puzzle*) (λ (c1) (λ (c2) (+ c1 c2)))))
+
+  (apply + (hash-values *beams*))
+
+
+  )
+
+
+(define (add-beam ht loc updater)
+  (if (hash-has-key? ht loc)
+      (hash-update ht loc updater)
+      (hash-set ht loc (updater 0))))
+
+
+;; propagate-beams needs a beam-updater procedure.
+;; beam-updater : number? -> number? -> number?
+;;
+;; beam-updater takes the counts of two beams which have merged and
+;; produces the new count for the merged beam. For part 1, that is
+;; always 1; for part 2, it is the sum of the individual beam counts.
+(define (propagate-beams beams splitter-rows beam-updater)
   (for/fold ([n 0]
              [beams beams])
             ([splitters (in-list splitter-rows)])
-    (for/list ([beam (in-list beams)])
-      ())
-    ))
+    (let-values ([(m split-beams)
+                  (for/fold ([n 0]
+                             [split-beams (make-immutable-hash)])
+                            ([(loc count) (in-hash beams)])
+                    (if (member loc splitters)
+                        (values
+                         (+ n 1)
+                         (add-beam
+                          (add-beam split-beams (+ loc 1) (beam-updater count))
+                          (- loc 1) (beam-updater count)))
+                        (values
+                         n
+                         (add-beam split-beams loc (beam-updater count)))))])
+      (println split-beams)
+      (values
+       (+ n m)
+       split-beams))))
 
 
 (define (find-splitters xs)
@@ -45,8 +88,23 @@
 ...............
 EOF
     )
-  
-  (with-input-from-string *input* parse-input)
 
+  (define *puzzle*
+    (with-input-from-string *input* parse-input))
+
+  ;; Part 1
+  (propagate-beams
+   (hash (caar *puzzle*) 1)      ; Starting column
+   (cdr *puzzle*)                ; List of locations of splitters on each row
+   (λ (count1) (λ (count2) 1)))  ; When combining beams, just maintain one
+
+  ;; Part 2
+  (define-values (_ beams)
+    (propagate-beams
+     (hash (caar *puzzle*) 1)
+     (cdr *puzzle*)
+     (λ (count1) (λ (count2) (+ count1 count2)))))
+  
+  (apply + (hash-values beams))
 
   )
